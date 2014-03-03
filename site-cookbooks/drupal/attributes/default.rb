6cb74de24c34a304c108ecf1aa82d61dbd7a1902
Willie Seabrook <willieseabrook@gmail.com>
2014-03-03 15:27:19 +0100

    - Bind mysql to localhost to make db connections more reliable

--- site-cookbooks/drupal/attributes/default.rb
+++ site-cookbooks/drupal/attributes/default.rb
@@ -4,3 +4,4 @@ override['apache']['default_site_enabled'] = false
 default['drush']['install_dir'] = "/usr/share/php/drush"
 default['drush']['version'] = "master"
 
+default['mysql']['bind_address'] = '127.0.0.1' 
