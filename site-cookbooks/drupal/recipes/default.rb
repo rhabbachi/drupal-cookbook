7559e1e1000966bfd1643b80c696d45eb3fd0c69
Riadh Habbachi <habbachi.riadh@gmail.com>
2014-03-17 12:09:31 +0100

    Code refactor on the default recipe.

--- site-cookbooks/drupal/recipes/default.rb
+++ site-cookbooks/drupal/recipes/default.rb
@@ -11,8 +11,13 @@ include_recipe "memcached"
 include_recipe "mysql::server"
 include_recipe "mysql::client"
 
-# Drupal environment
-%w{ libapache2-mod-php5 php5-curl php5-memcache php5-imagick php5-mysql libpcre3-dev }.each do |p|
+# PHP modules needed for drupal.
+php_modules = %w{ libapache2-mod-php5 php5-curl php5-memcache php5-imagick php5-mysql libpcre3-dev }
+# Pear modules needed for drupal.
+pear_modules = %w{ uploadprogress }
+
+# Process PHP modules.
+php_modules.each do |p|
     package p do
         action :install
     end
@@ -27,12 +32,13 @@ php_pear_channel 'pecl.php.net' do
     action :update
 end
 
-%w{ uploadprogress }.each do |p|
+pear_modules.each do |p|
     php_pear p do
         action :install
     end
 end
 
+# More pear modules with custom settings.
 php_pear "apc" do
     action :install
     directives(:shm_size => '64M', :enable_cli => 0)
