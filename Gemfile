6807d584cf49f65d0f72b05a7b4d9241d6157685
Riadh Habbachi <riadh@angrycactus.biz>
2014-12-05 11:29:23 +0100

    More fixes to make it work on Ubuntu 14.04
    * Updated Gemfile because of berkshelf bug.

--- Gemfile
+++ Gemfile
@@ -2,4 +2,9 @@ source 'https://rubygems.org'
 
 gem 'chef'
 gem 'knife-solo'
-gem 'berkshelf', '>=3.1.1'
+gem 'berkshelf', '>=3.1.4'
+
+group :plugins do
+  gem "vagrant-berkshelf"
+  gem "vagrant-omnibus"
+end
--- site-cookbooks/drupal/recipes/devel.rb
+++ site-cookbooks/drupal/recipes/devel.rb
@@ -6,24 +6,34 @@
 
 include_recipe "drupal::default"
 
-php_pear "xhprof" do
+if node['platform_version'] == '12.04'
+  php_pear "xhprof" do
     preferred_state "beta"
     action :install
-end
+  end
 
-php_pear "xdebug" do
+  php_pear "xdebug" do
     action :install
-end
+  end
 
-# php_pear zend_extensions doesn't work
-template "/etc/php5/conf.d/xdebug.ini" do
+  # php_pear zend_extensions doesn't work
+  template "/etc/php5/conf.d/xdebug.ini" do
     source "xdebug.ini.erb"
     owner "root"
     group "root"
     mode 0644
+  end
+else 
+  if node['platform_version'] == '14.04'
+    php_packages = %w{ php5-xhprof php5-xdebug }
+    php_packages.each do |p|
+      package p do
+        action :install
+      end
+    end
+  end
 end
 
 service "apache2" do
     action :restart
 end
-
